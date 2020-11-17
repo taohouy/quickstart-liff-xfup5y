2f1ad295134daf15b47a955ac9a180c23e1239c3
taohouy <taohouy@gmail.com>
2020-11-17 14:24:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quickstart-liff-xfup5y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quickstart-liff-xfup5y)
---
+++ index.js
@@ -0,0 +1,55 @@
+// Import stylesheets
+import "./style.css";
+
+// Body element
+const body = document.getElementById("body");
+
+// Button elements
+const btnSend = document.getElementById("btnSend");
+const btnClose = document.getElementById("btnClose");
+const btnShare = document.getElementById("btnShare");
+const btnLogIn = document.getElementById("btnLogIn");
+const btnLogOut = document.getElementById("btnLogOut");
+const btnScanCode = document.getElementById("btnScanCode");
+const btnOpenWindow = document.getElementById("btnOpenWindow");
+
+// Profile elements
+const email = document.getElementById("email");
+const userId = document.getElementById("userId");
+const pictureUrl = document.getElementById("pictureUrl");
+const displayName = document.getElementById("displayName");
+const statusMessage = document.getElementById("statusMessage");
+
+// QR element
+const code = document.getElementById("code");
+const friendShip = document.getElementById("friendShip");
+
+async function main() {
+  // Initialize LIFF app)
+  await liff.init({ liffId: "1655249088-4N2g5dgd" });
+  getUserProfile();
+  // Try a LIFF function
+  switch (liff.getOS()) {
+    case "android":
+      body.style.backgroundColor = "#d1f5d3";
+      break;
+    case "ios":
+      body.style.backgroundColor = "#eeeeee";
+      break;
+  }
+
+  if (!liff.isInClient()) {
+    btnLogIn.style.display = "block";
+    btnLogOut.style.display = "block";
+  }
+
+  async function getUserProfile() {
+    const profile = await liff.getProfile();
+    pictureUrl.src = profile.pictureUrl;
+    userId.innerHTML = "<b>userId:</b> " + profile.userId;
+    statusMessage.innerHTML = "<b>statusMessage:</b> " + profile.statusMessage;
+    displayName.innerHTML = "<b>displayName:</b> " + profile.displayName;
+    email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email;
+  }
+}
+main();
